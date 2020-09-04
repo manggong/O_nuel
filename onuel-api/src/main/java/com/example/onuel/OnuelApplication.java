f1a2c3a54bdbc979236eb037badfd4a2042fa92c
manggong <jihwank1995@gmail.com>
2020-09-04 21:22:47 +0900

    first_commit

---
+++ onuel-api/src/main/java/com/example/onuel/OnuelApplication.java
@@ -0,0 +1,13 @@
+package com.example.onuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnuelApplication.class, args);
+	}
+
+}
